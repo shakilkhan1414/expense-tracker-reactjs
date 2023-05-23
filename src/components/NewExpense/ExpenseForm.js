e4248babbe68c7ba055ea436bf929d28a5b345a9
shakilkhan1414 <khan.shakil.1414@gmail.com>
2023-05-23 23:14:14 +0600

    add expense items and add expense

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,25 @@
+import './ExpenseForm.css'
+
+export const ExpenseForm = () => {
+  return (
+    <form>
+        <div className="new-expense__controls">
+            <div className="new-expense__control">
+                <label>Title</label>
+                <input type="text" />
+            </div>
+            <div className="new-expense__control">
+                <label>Amount</label>
+                <input type="text" />
+            </div>
+            <div className="new-expense__control">
+                <label>Date</label>
+                <input type="date" />
+            </div>
+        </div>
+        <div className="new-expense__actions">
+            <button type="submit">Add Expense</button>
+        </div>
+    </form>
+  )
+}
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,11 @@
+import './NewExpense.css'
+import { ExpenseForm } from './ExpenseForm'
+
+export const NewExpense = () => {
+  return (
+    <div className='new-expense'>
+        <ExpenseForm />
+    </div>
+  )
+}
+
